c9a608eb71bec26e6d81fbc6ffb9f865adba96fe
Jeremy McMillan <jeremy.mcmillan@sap.com>
2015-03-23 11:42:47 -0500

    fix padding

--- datasize/__data_size__.py
+++ datasize/__data_size__.py
@@ -125,6 +125,7 @@ class DataSize(int):
         multiple = 1
         auto_modes = ('a', 'A')
         prefix_units = self.prefix_units
+        suffix_rpad_spaces = 0
         if not code or code[-1] == 'a':
             fmt_mode = 'a'
         elif code[-1] == 'A':
@@ -136,8 +137,10 @@ class DataSize(int):
         if fmt_mode in auto_modes:  # automatically choose a denomination/unit
             if fmt_mode == 'A':
                 prefix_units = self.nonstandard_units
+                suffix_rpad_spaces = max([len(k) for k in prefix_units.values()])
             else:
                 base_unit = 'B'
+                suffix_rpad_spaces = max([len(k) for k in prefix_units.values()]) + 1
             code = code.rstrip(''.join(auto_modes))
             denominations = list(prefix_units.keys())
             denominations.sort(reverse=True)
@@ -149,6 +152,7 @@ class DataSize(int):
         else:
             if code[-1] in ('b', 'B'):
                 base_unit = code[-1]
+                suffix_rpad_spaces += 1
                 code = code[:-1]  # eat the base unit
                 if base_unit == 'b':
                     multiple = self.word_length
@@ -158,6 +162,7 @@ class DataSize(int):
             for prefix in units:
                 offset = len(prefix)
                 if code[-offset:] == prefix:
+                    suffix_rpad_spaces += offset
                     code = code[:-offset]
                     denomination = self.unit_prefixes[prefix]
                     break
@@ -166,13 +171,19 @@ class DataSize(int):
         value = float(self * multiple)/float(denomination)
         
         if value.is_integer():  # emit integers if we can do it cleanly
+            code = code.split('.', 1)[0]  # if there is precision in the code, strip it`
+            code = '{c}{n}'.format(c=code[0], n=int(code) - suffix_rpad_spaces)
             code += 'd'
             cast = lambda x: int(x)
-            code = code.split('.', 1)[0]  # if there is precision in the code, strip it`
         else:
+            fpad, fprecision = code.split('.',1)
+            code = '{c}{pad}.{prec}'.format(c=fpad[0], pad=int(fpad) - suffix_rpad_spaces, prec=fprecision)
             code += 'f'
             cast = lambda x: x
-        format_parms = {'code': code, 'unit': prefix + base_unit}
+        unit_suffix_template = '{{:<{n}}}'.format(n=suffix_rpad_spaces)
+        print(unit_suffix_template)
+        unit_output_suffix = unit_suffix_template.format(prefix + base_unit)
+        format_parms = {'code': code, 'unit': unit_output_suffix}
         template = '{{:{code}}}{unit}'.format(**format_parms)
         return template.format(cast(float(value)))
 
