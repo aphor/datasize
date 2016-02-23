b6404ef89eec6bc4ce40527cccb6efee27f24006
Jeremy McMillan <aphor@aphor.net>
2016-02-23 07:56:42 -0600

    PEP8 cleanup

--- datasize/__datasize__.py
+++ datasize/__datasize__.py
@@ -1,15 +1,17 @@
 from math import ceil
+import sys
+
 
 def __bits_to_bytes__(b, word_length=8):
     B = ceil(b/word_length)
     return int(B)
 
-import sys
 
 if sys.version_info[0] < 3:
     __data_size_super__ = long
 else:
     __data_size_super__ = int
+
 
 class DataSize(__data_size_super__):
     '''Integer subclass that handles units appropriate for data allocation.
@@ -18,17 +20,17 @@ class DataSize(__data_size_super__):
     Internally represents data amounts as an integer count of bytes.
     Parses strings given as constructor values, and also provides
     string.format() support for human-readable data quantities expressed
-      in metric and IEC unit multiples of 
-        bytes: (suffix ending in 'B') or 
+      in metric and IEC unit multiples of
+        bytes: (suffix ending in 'B') or
         bits: (suffix ending in 'b')
       like 10.4TB or 128kb
     The minimum granularity is in bytes, defined as 8 bit words by default.
     Objects constructed from non integers will be rounded up to the nearest
       byte.
-    
+
     Arithmetic methods inherit directly from int, and return int. This
       keeps this class smaller, and avoids unecessary constructor overhead.
-    
+
     WARNING: in Python 2, DataSize is a subclass of long to avoid overflows
       on large values. Upgrade!
     '''
@@ -36,14 +38,14 @@ class DataSize(__data_size_super__):
     bit_suffix, byte_suffix = 'b', 'B'
     metric_prefixes = {
         # metric/decimal unit prefixes
-        'k' : 1000,
-        'M' : 1000**2,
-        'G' : 1000**3,
-        'T' : 1000**4,
-        'P' : 1000**5,
-        'E' : 1000**6,
-        'Z' : 1000**7,
-        'Y' : 1000**8,
+        'k': 1000,
+        'M': 1000**2,
+        'G': 1000**3,
+        'T': 1000**4,
+        'P': 1000**5,
+        'E': 1000**6,
+        'Z': 1000**7,
+        'Y': 1000**8,
         }
     IEC_prefixes = {
         # binary IEC unit prefixes
@@ -56,15 +58,21 @@ class DataSize(__data_size_super__):
         'Zi': 1024**7,
         'Yi': 1024**8,
         }
-    nonstandard_prefixes = dict(zip((k[0] for k in IEC_prefixes.keys()),(m for m in IEC_prefixes.values())))
+    nonstandard_prefixes = dict(zip(
+            (k[0] for k in IEC_prefixes.keys()),
+            (m for m in IEC_prefixes.values())
+            ))
     unit_prefixes = metric_prefixes.copy()
     unit_prefixes.update(IEC_prefixes)
     # also make a map from unit denominations to prefix
-    prefix_units = dict(zip( tuple(unit_prefixes.values()), tuple(unit_prefixes.keys())) )
-    nonstandard_units = dict(zip((m for m in IEC_prefixes.values()),(k[0] for k in IEC_prefixes.keys())))
-    
+    prefix_units = dict(zip(
+        tuple(unit_prefixes.values()), tuple(unit_prefixes.keys())))
+    nonstandard_units = dict(zip(
+        (m for m in IEC_prefixes.values()),
+        (k[0] for k in IEC_prefixes.keys())))
+
     def __init__(self, spec, word_length=8):
-        '''Usage: 
+        '''Usage:
         min_heap = DataSize('768Mib')
         max_heap = DataSize('2G')
         max_heap - min_heap = high_memory_warning_limit
@@ -76,11 +84,11 @@ class DataSize(__data_size_super__):
         to specify some other bits per byte than the default of 8.
         '''
         self.word_length = int(word_length)
-        
+
     def __new__(subclass, spec, **kwargs):
         '''Because DataSize is a subclass of int, we must override __new__()
-        to implement a string decoder that can provide an immutable integer value
-        for instances.
+        to implement a string decoder that can provide an immutable integer
+        value for instances.
         '''
         word_length = int(kwargs.get('word_length', DataSize.word_length))
         unit = 'bytes'
@@ -108,17 +116,17 @@ class DataSize(__data_size_super__):
         else:
             bits = raw_number * word_length * multiple
             value = raw_number * multiple
-        if type(value) == type(float(0)):
+        if isinstance(value, float):
             value = int(ceil(value))
         return __data_size_super__.__new__(subclass, value)
 
     def __format__(self, code):
-        '''formats as a decimal number, but recognizes data units as type format codes.
-        Precision is ignored for integer multiples of the unit specified in the format code.
-        format codes:  
+        '''formats as a decimal number, but recognizes data units as type
+        format codes.Precision is ignored for integer multiples of the unit
+        specified in the format code.format codes:
         a    autoformat will choose a unit defaulting to the largest
               size with a quantity >= 1 (default)
-        A    abbreviated number of bytes (implied IEC units, and implied 'B' bytes suffix omitted)
+        A    abbreviated number of bytes (implied IEC units of 'B' bytes)
         B    bytes      (1)
         kiB  kibibytes  (1024)
         kB   kilobytes  (1000)
@@ -128,9 +136,10 @@ class DataSize(__data_size_super__):
         ...
         YiB  Yobibytes  (1024**8)
         YB   Yottabytes (10**24)
-        
+
         >>> from datasize import DataSize
-        >>> 'My new {:GB} SSD really only stores {:.2GiB} of data.'.format(DataSize('750GB'),DataSize(DataSize('750GB') * 0.8))
+        >>> 'My new {:GB} SSD really only stores {:.2GiB} of data.'.format(
+                DataSize('750GB'),DataSize(DataSize('750GB') * 0.8))
         'My new 750GB SSD really only stores 558.79GiB of data.'
         '''
         base_unit = ''
@@ -151,10 +160,14 @@ class DataSize(__data_size_super__):
         if fmt_mode in auto_modes:  # automatically choose a denomination/unit
             if fmt_mode == 'A':
                 prefix_units = self.nonstandard_units
-                suffix_rpad_spaces = max([len(k) for k in prefix_units.values()])
+                suffix_rpad_spaces = max(
+                        [len(k) for k in prefix_units.values()]
+                    )
             else:
                 base_unit = 'B'
-                suffix_rpad_spaces = max([len(k) for k in prefix_units.values()]) + 1
+                suffix_rpad_spaces = 1 + max(
+                        [len(k) for k in prefix_units.values()]
+                    )
             code = code.rstrip(''.join(auto_modes))
             denominations = list(prefix_units.keys())
             denominations.sort(reverse=True)
@@ -170,7 +183,7 @@ class DataSize(__data_size_super__):
                 code = code[:-1]  # eat the base unit
                 if base_unit == 'b':
                     multiple = self.word_length
-            
+
             units = list(self.unit_prefixes.keys())
             units.sort(reverse=True)
             for prefix in units:
@@ -183,13 +196,18 @@ class DataSize(__data_size_super__):
         if denomination > 1 and not base_unit and fmt_mode != 'A':
             base_unit = 'B'
         value = float(self * multiple)/float(denomination)
-        
+
         if value.is_integer():  # emit integers if we can do it cleanly
-            code = code.split('.', 1)[0]  # if there is precision in the code, strip it
+            code = code.split('.', 1)[0]  # precision in the code? strip it
             if code:
-                code = '{c}{n}'.format(c=code[0], n=(int(code) - suffix_rpad_spaces))
+                code = '{c}{n}'.format(
+                                        c=code[0],
+                                        n=(int(code) - suffix_rpad_spaces))
             code += 'd'
-            cast = lambda x: int(x)
+            
+            def cast(x):
+                return int(x)
+
         else:
             if code and '.' in code:
                 fpad, fprecision = code.split('.',1)
@@ -201,9 +219,15 @@ class DataSize(__data_size_super__):
                     npad = int(fpad) - suffix_rpad_spaces
                 else:
                     npad = ''
-                code = '{c}{pad}.{prec}'.format(c=padchar, pad=npad, prec=fprecision)
+                code = '{c}{pad}.{prec}'.format(
+                                                c=padchar,
+                                                pad=npad,
+                                                prec=fprecision)
             code += 'f'
-            cast = lambda x: x
+            
+            def cast(x):
+                return x
+
         unit_suffix_template = '{{:<{n}}}'.format(n=suffix_rpad_spaces)
         unit_output_suffix = unit_suffix_template.format(prefix + base_unit)
         format_parms = {'code': code, 'unit': unit_output_suffix}
