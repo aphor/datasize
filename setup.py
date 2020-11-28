4211e2b6e562b934c12a1dfe71343347195f7e70
Jeremy McMillan <jeremy.mcmillan@gmail.com>
2020-11-28 17:45:59 -0600

    version rev 1.0.0

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
+
+with open("README.md", "r") as desc_file:
+    long_description = desc_file.read()
+
 setup(
   name = 'datasize',
   packages = ['datasize'],
-  version = '0.3',
+  version = '1.0.0',
   description = 'Python integer subclass to handle arithmetic and formatting of integers with data size units',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Jeremy McMillan',
   author_email = 'jeremy.mcmillan@gmail.com',
   url = 'https://github.com/aphor/datasize',
-  download_url = 'https://github.com/aphor/datasize/tarball/0.1',
+  download_url = 'https://github.com/aphor/datasize/tarball/1.0.0',
   keywords = ['data', 'units', 'parser', 'formatter'],
   classifiers = [],
 )
